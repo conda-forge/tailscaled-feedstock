82cfd412b7278663f48b7907f1f3d9ad9ce3701f
Oliver Borchert <me@borchero.com>
2024-05-04 20:52:41 +0200

    Add additional version information to binaries

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,14 @@
 @echo on
 
-cd cmd/tailscaled
+cd %SRC_DIR%/git
+for /f "delims=" %%a IN ('git rev-parse HEAD') do set GIT_COMMIT_SHA=%%a
+set "GIT_SHORT_SHA=!GIT_COMMIT_SHA:~0,9!"
+
+cd %SRC_DIR%/release/cmd/tailscaled
 
 go-licenses save . ^
     --save_path ../../library_licenses
 
 go install -v ^
-    -ldflags "-s -w -X 'tailscale.com/version.shortStamp=v%PKG_VERSION%'" ^
+    -ldflags "-s -w -X 'tailscale.com/version.shortStamp=v%PKG_VERSION%' -X 'tailscale.com/version.longStamp=%PKG_VERSION%-t%GIT_SHORT_SHA%' -X 'tailscale.com/version.gitCommitStamp=%GIT_COMMIT_SHA%'" ^
     .
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,14 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cd cmd/tailscaled
+GIT_COMMIT_SHA=$(cd $SRC_DIR/git && git rev-parse HEAD)
+GIT_SHORT_SHA=${GIT_COMMIT_SHA:0:9}
+
+cd $SRC_DIR/release/cmd/tailscaled
 
 go-licenses save . \
     --save_path ../../library_licenses
 
 go build -v \
-    -ldflags "-s -w -X 'tailscale.com/version.shortStamp=$PKG_VERSION'" \
+    -ldflags "-s -w -X 'tailscale.com/version.shortStamp=$PKG_VERSION' -X 'tailscale.com/version.longStamp=$PKG_VERSION-t$GIT_SHORT_SHA' -X 'tailscale.com/version.gitCommitStamp=$GIT_COMMIT_SHA'" \
     -o $PREFIX/bin/tailscaled
